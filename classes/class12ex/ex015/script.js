02e8cdc90c8d32867681d861e023aaaea7ebdc15
Lauro Henrique Brant <brantlauro@gmail.com>
2021-02-17 14:11:43 -0300

    english translate

--- classes/class12ex/ex015/script.js
+++ classes/class12ex/ex015/script.js
@@ -1,60 +1,45 @@
-function verificar() {
+function verify() {
+    var date = new Date();
+    var year = date.getFullYear();
+    var fyear = document.getElementById("txtyear");
+    var ans = document.querySelector("div#ans");
 
-    var data = new Date();
-
-    var ano = data.getFullYear();
-
-    var fano = document.getElementById("txtano");
-
-    var res = document.querySelector("div#res");
-
-    if(fano.value.length == 0 || Number(fano.value) > ano) {
-
-        window.alert('[ERROR] Verifique os dados e tente novamente!');
-
+    if(fyear.value.length == 0 || Number(fyear.value) > year) {
+        window.alert('[ERROR] Check the data and try again!');
     }else{
-
-        var fsex = document.getElementsByName("radsex");
-
-        var idade = ano - Number(fano.value);
-
-        var genero = "";
-
+        var fgen = document.getElementsByName("radgen");
+        var age = year - Number(fyear.value);
+        var genre = "";
         var img = document.createElement('img');
+        img.setAttribute('id', 'photo');
 
-        img.setAttribute('id', 'foto');
-
-        if(fsex[0].checked) {
-
-            genero = 'Homem';
-
-            if(idade <= 11){
-                img.setAttribute('src', 'criançahomem.png');
-            }else if(idade < 18){
-                img.setAttribute('src', 'jovemhomem.png');
-            }else if (idade < 60){
-                img.setAttribute('src', 'homem.png');
+        if(fgen[0].checked) {
+            genre = 'Man';
+            if(age <= 11){
+                img.setAttribute('src', 'kid-man.png');
+            }else if(age < 18){
+                img.setAttribute('src', 'young-man.png');
+            }else if (age < 60){
+                img.setAttribute('src', 'man.png');
             }else{
-                img.setAttribute('src', 'idosohomem.png');
+                img.setAttribute('src', 'old-man.png');
             }
 
         }else{
-
-            genero = 'Mulher';
-
-            if(idade <= 11){
-                img.setAttribute('src', 'criançamulher.png');
-            }else if(idade < 18){
-                img.setAttribute('src', 'jovemmulher.png');
-            }else if(idade < 60){
-                img.setAttribute('src', 'mulher.png');
+            genre = 'Woman';
+            if(age <= 11){
+                img.setAttribute('src', 'kid-woman.png');
+            }else if(age < 18){
+                img.setAttribute('src', 'young-woman.png');
+            }else if(age < 60){
+                img.setAttribute('src', 'woman.png');
             }else{
-                img.setAttribute('src', 'idosamulher.png');
+                img.setAttribute('src', 'old-woman.png');
             }
         }
 
-        res.style.textAlign = 'center';
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos!`;
-        res.appendChild(img);
+        ans.style.textAlign = 'center';
+        ans.innerHTML = `You are ${genre} with ${age} year old!`;
+        ans.appendChild(img);
     } 
 }
